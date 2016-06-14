671b2509b846f0670e1264f4db2077371e9615aa
raihan-mpower <raihan@mpower-social.com>
2016-06-14 14:09:42 +0600

    RA| stripped down apk size by introducing build variants

--- opensrp-app/src/main/java/org/ei/opensrp/view/fragment/DisplayFormFragment.java
+++ opensrp-app/src/main/java/org/ei/opensrp/view/fragment/DisplayFormFragment.java
@@ -117,6 +117,7 @@ public class DisplayFormFragment extends Fragment {
 //        webView.getSettings().setGeolocationEnabled(true);
 //        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         XwebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+//        XwebView.
 //
 //        webView.setWebViewClient(new AppWebViewClient(progressBar));
 //        webView.setWebChromeClient(new WebChromeClient() {
