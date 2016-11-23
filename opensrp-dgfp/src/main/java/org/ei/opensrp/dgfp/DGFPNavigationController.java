c3dd0e4cba2ba10ff0e739c6b7f0f9557d528dc8
raihan-mpower <raihan@mpower-social.com>
2016-11-23 19:18:42 +0600

    RA|setting up home registry

---
+++ opensrp-dgfp/src/main/java/org/ei/opensrp/dgfp/DGFPNavigationController.java
@@ -0,0 +1,33 @@
+package org.ei.opensrp.dgfp;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import org.ei.opensrp.view.controller.ANMController;
+
+public class DGFPNavigationController extends org.ei.opensrp.view.controller.NavigationController {
+    private Activity activity;
+    private ANMController anmController;
+
+    public DGFPNavigationController(Activity activity, ANMController anmController) {
+        super(activity,anmController);
+        this.activity = activity;
+        this.anmController = anmController;
+    }
+    @Override
+    public void startECSmartRegistry() {
+
+//        activity.startActivity(new Intent(activity, HH_member_SmartRegisterActivity.class));
+
+
+    }
+    @Override
+    public void startFPSmartRegistry() {
+//        activity.startActivity(new Intent(activity, HH_woman_member_SmartRegisterActivity.class));
+    }
+    @Override
+    public void startChildSmartRegistry() {
+//        activity.startActivity(new Intent(activity, HH_child_member_SmartRegisterActivity.class));
+    }
+
+}
